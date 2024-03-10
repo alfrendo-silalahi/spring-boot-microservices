fe65d9366c98e0bbc40f922d90c3be40c7dcd5f7
Alfrendo Silalahi <alfrendos72@gmail.com>
2024-03-10 16:17:57 +0700

    Add DTO

---
+++ accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDTO.java
@@ -0,0 +1,14 @@
+package com.eazybytes.accounts.dto;
+
+import lombok.Data;
+
+@Data
+public class AccountsDTO {
+
+    private long accountNumber;
+
+    private String accountType;
+
+    private String branchAddress;
+
+}
